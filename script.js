e88c428a3e0ecec1f1d862b64dd157d93737c0b0
Rafael Silva Willians <rafael.willians@outlook.com>
2024-12-28 21:30:22 +0000

    Adicionado disparo click sair popup

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
 }
+
+exitBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+}
+
