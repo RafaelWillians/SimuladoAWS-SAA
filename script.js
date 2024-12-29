f9f48ae2ad6a641f0de126afa43b06b059896b71
Rafael Silva Willians <rafael.willians@outlook.com>
2024-12-29 00:43:39 +0000

    Adicionado mecanismo carregar perguntas via lista questions

--- script.js
+++ script.js
@@ -23,14 +23,48 @@ continueBtn.onclick = () => {
     quizBox.classList.add('active');
 
     showQuestions(0);
+    questionCounter(1);
 }
 
 let questionCount = 0;
+let questionNumb = 1;
 
-const nextBtn = 
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    if(questionCount < questions.length - 1) {
+        questionCount++;
+        showQuestions(questionCount);
+        
+        questionNumb++;
+        questionCounter(questionNumb);
+    }
+    else {
+        console.log('Quiz finalizado');
+    }
+}
+
+const optionList = document.querySelector('.option-list');
 
 // carregar as questoes do array
 function showQuestions(index) {
     const questionText = document.querySelector('.question-text');
     questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+        <div class="option"><span>${questions[index].options[1]}</span></div>
+        <div class="option"><span>${questions[index].options[2]}</span></div>
+        <div class="option"><span>${questions[index].options[3]}</span></div>`;
+    
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+}
+
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} de ${questions.length} Perguntas`;
 }
