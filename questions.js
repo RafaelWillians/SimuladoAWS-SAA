f79eff9138e324e47f524783916d21b020830b0e
Rafael Silva Willians <rafael.willians@outlook.com>
2024-12-29 14:41:46 +0000

    Alteradas questoes para teste

--- questions.js
+++ questions.js
@@ -2,7 +2,7 @@ let questions = [
     {
         numb: 1,
         question: "Pergunta teste 1?",
-        answer: "B. Resposta 2",
+        answer: "A. Resposta 1",
         options: [
             "A. Resposta 1",
             "B. Resposta 2",
@@ -13,7 +13,7 @@ let questions = [
     {
         numb: 2,
         question: "Pergunta teste 2?",
-        answer: "D. Resposta 4",
+        answer: "B. Resposta 1",
         options: [
             "A. Resposta 1",
             "B. Resposta 2",
@@ -24,7 +24,7 @@ let questions = [
     {
         numb: 3,
         question: "Pergunta teste 3?",
-        answer: "A. Resposta 1",
+        answer: "C. Resposta 3",
         options: [
             "A. Resposta 1",
             "B. Resposta 2",
@@ -35,7 +35,7 @@ let questions = [
     {
         numb: 4,
         question: "Pergunta teste 4?",
-        answer: "B. Resposta 2",
+        answer: "D. Resposta 4",
         options: [
             "A. Resposta 1",
             "B. Resposta 2",
